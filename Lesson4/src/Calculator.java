94b88bd9b6d3b2c3c949506fcdabebddf22e8088
Satellite-PC <Satellite-PC@TOSHIBA-SATELLITE.mshome.net>
2018-10-26 13:00:32 +0200

    My first application

---
+++ Lesson4/src/Calculator.java
@@ -0,0 +1,42 @@
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+		System.out.println("answer = " + addition());
+		System.out.println("answer = " + subtraction());
+		System.out.println("answer = " + division ());
+		System.out.println("answer = " + multiply());
+		
+	}
+	
+	public static int addition() {
+		
+		
+		return 20+10;
+		
+	}
+	
+	public static int subtraction() {
+		
+		return 50-20;
+		
+		
+		
+	}
+	public static int division() {
+		
+
+		return 100/50;
+		
+	}
+	public static int multiply() {
+		
+		return 20*4;
+	}
+	
+}
+
+
